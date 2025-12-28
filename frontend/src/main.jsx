5d16b26d26b6f0b25d2062feaaad99d48db5d6f3
meeranxai <meeran01110rafiq@gmail.com>
2025-12-28 18:29:37 +0500

    SIMPLE TEST: Basic React app to test deployment - No complex dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,38 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { AuthProvider } from './contexts/AuthContext'
-
-// Global Styles - Design System First (Foundation)
-import './styles/design-system.css'
-import './styles/style.css'
-import './styles/social.css'
-import './styles/components.css'
-import './styles/profile.css'
-import './styles/whatsapp.css'
-import './styles/call.css'
-import './styles/app-integration.css' // Global overrides & integration
-
-
-// FontAwesome (You might want to install this via npm later, but for now CDN in index.html or import here if npm installed)
-// For verify, we'll assume the index.html template has it or we can import if downloaded. 
-// Actually, let's inject the CDN link in index.html of Vite or just trust legacy styles if they used @import.
-// Better practice: Add CDN to Vite's index.html
-
-import { SocketProvider } from './contexts/SocketContext'
-import { NotificationProvider } from './contexts/NotificationContext'
-
-// PWA Support
-import './utils/pwa-install.js'
+import App from './App-simple.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <AuthProvider>
-            <SocketProvider>
-                <NotificationProvider>
-                    <App />
-                </NotificationProvider>
-            </SocketProvider>
-        </AuthProvider>
+        <App />
     </React.StrictMode>,
 )
